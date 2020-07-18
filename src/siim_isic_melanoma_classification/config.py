013114482fb2766570dbe13adadb840207cda7f4
Masahiro Wada <argon.argon.argon@gmail.com>
2020-07-19 03:48:29 +0900

    Change default batch_size to 32

--- src/siim_isic_melanoma_classification/config.py
+++ src/siim_isic_melanoma_classification/config.py
@@ -15,7 +15,7 @@ class Config:
 
 
 def get_config() -> Config:
-    batch_size = int(os.environ.get("KAGGLE_BATCH_SIZE", 2))
+    batch_size = int(os.environ.get("KAGGLE_BATCH_SIZE", 32))
     learning_rate = float(os.environ.get("KAGGLE_LEARNING_RATE", 0.008))
     num_workers = int(os.environ.get("KAGGLE_NUM_WORKERS", 4))
     max_epochs = int(os.environ.get("KAGGLE_MAX_EPOCHS", 4))
