ccd21af4ff838e8f5accf5c0c7d76529c66ae0d1
Masahiro Wada <argon.argon.argon@gmail.com>
2020-08-06 18:01:51 +0900

    Add en b4

--- src/siim_isic_melanoma_classification/net.py
+++ src/siim_isic_melanoma_classification/net.py
@@ -13,6 +13,7 @@ def load_from_checkpoint(model_name, path_to_cktp: Path) -> LightningModelBase:
         EfficientNetB0MLP.name: EfficientNetB0MLP,
         EfficientNetB2MLP.name: EfficientNetB2MLP,
         EfficientNetB3MLP.name: EfficientNetB3MLP,
+        EfficientNetB4MLP.name: EfficientNetB4MLP,
         EfficientNetB6MLP.name: EfficientNetB6MLP,
     }[model_name].load_from_checkpoint(str(path_to_cktp))
 
@@ -169,4 +170,43 @@ class EfficientNetB6MLP(LightningModelBase):
         features = self.features(inputs)
         output = self._classifier(features)
         return output.view(output.size(0), -1)
+
+
+class EfficientNetB4MLP(LightningModelBase):
+    name: str = "en_b4_mlp"
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config, **kwargs)
+        self._backbone = timm.create_model(
+            "tf_efficientnet_b4_ns", num_classes=512, pretrained=True
+        )
+        self._meta = nn.Sequential(
+            nn.Linear(9, 512),
+            nn.BatchNorm1d(512),
+            Swish(),
+            nn.Dropout(p=0.2),
+            nn.Linear(512, 256),  # FC layer output will have 256 features
+            nn.BatchNorm1d(256),
+            Swish(),
+            nn.Dropout(p=0.2),
+        )
+        self._classifier = nn.Linear(512 + 256, 1)
+
+    def backbone(self, x):
+        return self._backbone(x)
+
+    def meta(self, x):
+        return self._meta(x)
+
+    def features(self, inputs):
+        x, y = inputs
+        x = self.backbone(x)
+        y = self.meta(y)
+        return torch.cat((x, y), dim=1)
+
+    def forward(self, inputs):
+        features = self.features(inputs)
+        output = self._classifier(features)
+        return output.view(output.size(0), -1)
+
 
