bb7eaac52981dd2c60118991b0842b7765ea7542
Masahiro Wada <argon.argon.argon@gmail.com>
2020-08-06 20:29:25 +0900

    Update setup_dataset.sh

--- script/setup_dataset.sh
+++ script/setup_dataset.sh
@@ -1,13 +1,30 @@
-kaggle d download "cdeotte/jpeg-melanoma-128x128"
-kaggle d download "cdeotte/jpeg-melanoma-256x256"
-kaggle d download "cdeotte/jpeg-melanoma-384x384"
-kaggle d download "cdeotte/jpeg-isic2019-128x128"
-kaggle d download "cdeotte/jpeg-isic2019-256x256"
-kaggle d download "cdeotte/jpeg-isic2019-384x384"
-kaggle d download "nroman/melanoma-hairs"
-kaggle d download "ar90ngas/timm-pretrained-efficientnet"
-kaggle d download "ar90ngas/my-isic2020-csv"
-kaggle d download "ar90ngas/my-isic2020-experiments"
-kaggle d download "cdeotte/malignant-v2-128x128"
-kaggle d download "cdeotte/malignant-v2-256x256"
-kaggle d download "cdeotte/malignant-v2-384x384"
+#!/usr/bin/env bash
+PROJ_ROOT=$(cd $(dirname $0); pwd)/../
+INPUT_DIR=${PROJ_ROOT}/../input
+
+function setup_dataset () {
+    SLUG=$(echo ${1} | sed 's/\/.*//')
+    DATANAME=$(echo ${1} | sed 's/.*\///')
+    mkdir -p ${INPUT_DIR}/${DATANAME}
+    pushd ${INPUT_DIR}/${DATANAME}
+    kaggle d download ${1}
+    unzip ${DATANAME}.zip
+    rm ${DATANAME}.zip
+    popd
+}
+
+mkdir -p $INPUT_DIR
+
+setup_dataset "cdeotte/jpeg-melanoma-128x128"
+setup_dataset "cdeotte/jpeg-melanoma-256x256"
+setup_dataset "cdeotte/jpeg-melanoma-384x384"
+setup_dataset "cdeotte/jpeg-isic2019-128x128"
+setup_dataset "cdeotte/jpeg-isic2019-256x256"
+setup_dataset "cdeotte/jpeg-isic2019-384x384"
+setup_dataset "nroman/melanoma-hairs"
+setup_dataset "ar90ngas/timm-pretrained-efficientnet"
+setup_dataset "ar90ngas/my-isic2020-csv"
+setup_dataset "ar90ngas/my-isic2020-experiments"
+setup_dataset "cdeotte/malignant-v2-128x128"
+setup_dataset "cdeotte/malignant-v2-256x256"
+setup_dataset "cdeotte/malignant-v2-384x384"
