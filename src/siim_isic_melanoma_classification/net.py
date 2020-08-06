f99876c4e0b0bb4220230c01ae09c89cefbe464a
Masahiro Wada <argon.argon.argon@gmail.com>
2020-08-07 01:55:16 +0900

    Add efficientnet mlp factory

--- src/siim_isic_melanoma_classification/net.py
+++ src/siim_isic_melanoma_classification/net.py
@@ -8,6 +8,66 @@ import torch
 from .lightning import LightningModelBase
 
 
+def _build_en_mlp_class(model_level: int):
+    pretrained_model_name = [
+        "efficientnet_b0",
+        "efficientnet_b1",
+        "efficientnet_b2a",
+        "efficientnet_b3",
+        "tf_efficientnet_b4_ns",
+        "tf_efficientnet_b5_ns",
+        "tf_efficientnet_b6_ns",
+    ][model_level]
+
+    class _c(LightningModelBase):
+        name: str = f"en_b{model_level}_mlp"
+
+        def __init__(self, config, **kwargs):
+            super().__init__(config, **kwargs)
+            self._backbone = timm.create_model(
+                pretrained_model_name, num_classes=512, pretrained=True
+            )
+            self._meta = nn.Sequential(
+                nn.Linear(9, 512),
+                nn.BatchNorm1d(512),
+                Swish(),
+                nn.Dropout(p=0.2),
+                nn.Linear(512, 256),  # FC layer output will have 256 features
+                nn.BatchNorm1d(256),
+                Swish(),
+                nn.Dropout(p=0.2),
+            )
+            self._classifier = nn.Linear(512 + 256, 1)
+
+        def backbone(self, x):
+            return self._backbone(x)
+
+        def meta(self, x):
+            return self._meta(x)
+
+        def features(self, inputs):
+            x, y = inputs
+            x = self.backbone(x)
+            y = self.meta(y)
+            return torch.cat((x, y), dim=1)
+
+        def forward(self, inputs):
+            features = self.features(inputs)
+            output = self._classifier(features)
+            return output.view(output.size(0), -1)
+
+    return _c
+
+
+EfficientNetB0MLP = _build_en_mlp_class(0)
+EfficientNetB1MLP = _build_en_mlp_class(1)
+EfficientNetB2MLP = _build_en_mlp_class(2)
+EfficientNetB3MLP = _build_en_mlp_class(3)
+EfficientNetB4MLP = _build_en_mlp_class(4)
+EfficientNetB5MLP = _build_en_mlp_class(5)
+EfficientNetB6MLP = _build_en_mlp_class(6)
+
+
 def load_from_checkpoint(model_name, path_to_cktp: Path) -> LightningModelBase:
     return {
         EfficientNetB0MLP.name: EfficientNetB0MLP,
@@ -17,235 +77,3 @@ def load_from_checkpoint(model_name, path_to_cktp: Path) -> LightningModelBase:
         EfficientNetB4MLP.name: EfficientNetB4MLP,
         EfficientNetB6MLP.name: EfficientNetB6MLP,
     }[model_name].load_from_checkpoint(str(path_to_cktp))
-
-
-class EfficientNetB2MLP(LightningModelBase):
-    name: str = "en_b2_mlp"
-
-    def __init__(self, config, **kwargs):
-        super().__init__(config, **kwargs)
-        self._backbone = timm.create_model(
-            "efficientnet_b2a", num_classes=512, pretrained=True
-        )
-        self._meta = nn.Sequential(
-            nn.Linear(9, 512),
-            nn.BatchNorm1d(512),
-            Swish(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512, 256),  # FC layer output will have 256 features
-            nn.BatchNorm1d(256),
-            Swish(),
-            nn.Dropout(p=0.2),
-        )
-        self._classifier = nn.Linear(512 + 256, 1)
-
-    def backbone(self, x):
-        return self._backbone(x)
-
-    def meta(self, x):
-        return self._meta(x)
-
-    def features(self, inputs):
-        x, y = inputs
-        x = self.backbone(x)
-        y = self.meta(y)
-        return torch.cat((x, y), dim=1)
-
-    def forward(self, inputs):
-        features = self.features(inputs)
-        output = self._classifier(features)
-        return output.view(output.size(0), -1)
-
-
-
-
-class EfficientNetB0MLP(LightningModelBase):
-    name: str = "en_b0_mlp"
-
-    def __init__(self, config, **kwargs):
-        super().__init__(config, **kwargs)
-        self._backbone = timm.create_model(
-            "efficientnet_b0", num_classes=512, pretrained=True
-        )
-        self._meta = nn.Sequential(
-            nn.Linear(9, 512),
-            nn.BatchNorm1d(512),
-            Swish(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512, 256),  # FC layer output will have 256 features
-            nn.BatchNorm1d(256),
-            Swish(),
-            nn.Dropout(p=0.2),
-        )
-        self._classifier = nn.Linear(512 + 256, 1)
-
-    def backbone(self, x):
-        return self._backbone(x)
-
-    def meta(self, x):
-        return self._meta(x)
-
-    def features(self, inputs):
-        x, y = inputs
-        x = self.backbone(x)
-        y = self.meta(y)
-        return torch.cat((x, y), dim=1)
-
-    def forward(self, inputs):
-        features = self.features(inputs)
-        output = self._classifier(features)
-        return output.view(output.size(0), -1)
-
-
-class EfficientNetB3MLP(LightningModelBase):
-    name: str = "en_b3_mlp"
-
-    def __init__(self, config, **kwargs):
-        super().__init__(config, **kwargs)
-        self._backbone = timm.create_model(
-            "efficientnet_b3", num_classes=512, pretrained=True
-        )
-        self._meta = nn.Sequential(
-            nn.Linear(9, 512),
-            nn.BatchNorm1d(512),
-            Swish(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512, 256),  # FC layer output will have 256 features
-            nn.BatchNorm1d(256),
-            Swish(),
-            nn.Dropout(p=0.2),
-        )
-        self._classifier = nn.Linear(512 + 256, 1)
-
-    def backbone(self, x):
-        return self._backbone(x)
-
-    def meta(self, x):
-        return self._meta(x)
-
-    def features(self, inputs):
-        x, y = inputs
-        x = self.backbone(x)
-        y = self.meta(y)
-        return torch.cat((x, y), dim=1)
-
-    def forward(self, inputs):
-        features = self.features(inputs)
-        output = self._classifier(features)
-        return output.view(output.size(0), -1)
-
-
-class EfficientNetB6MLP(LightningModelBase):
-    name: str = "en_b6_mlp"
-
-    def __init__(self, config, **kwargs):
-        super().__init__(config, **kwargs)
-        self._backbone = timm.create_model(
-            "tf_efficientnet_b6_ns", num_classes=512, pretrained=True
-        )
-        self._meta = nn.Sequential(
-            nn.Linear(9, 512),
-            nn.BatchNorm1d(512),
-            Swish(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512, 256),  # FC layer output will have 256 features
-            nn.BatchNorm1d(256),
-            Swish(),
-            nn.Dropout(p=0.2),
-        )
-        self._classifier = nn.Linear(512 + 256, 1)
-
-    def backbone(self, x):
-        return self._backbone(x)
-
-    def meta(self, x):
-        return self._meta(x)
-
-    def features(self, inputs):
-        x, y = inputs
-        x = self.backbone(x)
-        y = self.meta(y)
-        return torch.cat((x, y), dim=1)
-
-    def forward(self, inputs):
-        features = self.features(inputs)
-        output = self._classifier(features)
-        return output.view(output.size(0), -1)
-
-
-class EfficientNetB4MLP(LightningModelBase):
-    name: str = "en_b4_mlp"
-
-    def __init__(self, config, **kwargs):
-        super().__init__(config, **kwargs)
-        self._backbone = timm.create_model(
-            "tf_efficientnet_b4_ns", num_classes=512, pretrained=True
-        )
-        self._meta = nn.Sequential(
-            nn.Linear(9, 512),
-            nn.BatchNorm1d(512),
-            Swish(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512, 256),  # FC layer output will have 256 features
-            nn.BatchNorm1d(256),
-            Swish(),
-            nn.Dropout(p=0.2),
-        )
-        self._classifier = nn.Linear(512 + 256, 1)
-
-    def backbone(self, x):
-        return self._backbone(x)
-
-    def meta(self, x):
-        return self._meta(x)
-
-    def features(self, inputs):
-        x, y = inputs
-        x = self.backbone(x)
-        y = self.meta(y)
-        return torch.cat((x, y), dim=1)
-
-    def forward(self, inputs):
-        features = self.features(inputs)
-        output = self._classifier(features)
-        return output.view(output.size(0), -1)
-
-
-class EfficientNetB1MLP(LightningModelBase):
-    name: str = "en_b1_mlp"
-
-    def __init__(self, config, **kwargs):
-        super().__init__(config, **kwargs)
-        self._backbone = timm.create_model(
-            "efficientnet_b1", num_classes=512, pretrained=True
-        )
-        self._meta = nn.Sequential(
-            nn.Linear(9, 512),
-            nn.BatchNorm1d(512),
-            Swish(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512, 256),  # FC layer output will have 256 features
-            nn.BatchNorm1d(256),
-            Swish(),
-            nn.Dropout(p=0.2),
-        )
-        self._classifier = nn.Linear(512 + 256, 1)
-
-    def backbone(self, x):
-        return self._backbone(x)
-
-    def meta(self, x):
-        return self._meta(x)
-
-    def features(self, inputs):
-        x, y = inputs
-        x = self.backbone(x)
-        y = self.meta(y)
-        return torch.cat((x, y), dim=1)
-
-    def forward(self, inputs):
-        features = self.features(inputs)
-        output = self._classifier(features)
-        return output.view(output.size(0), -1)
-
-
