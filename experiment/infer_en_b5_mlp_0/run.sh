874fff27db187a424c78a1e4b88b4edea9a25b12
Masahiro Wada <argon.argon.argon@gmail.com>
2020-08-07 03:22:40 +0000

    Add experiments

---
+++ experiment/infer_en_b5_mlp_0/run.sh
@@ -0,0 +1,15 @@
+#/usr/local/bin/bash
+unset $(env | grep KKT | awk -F = '{print $1}')
+
+export KAGGLE_GPUS=1
+#export KAGGLE_TPUS=1
+export KAGGLE_BATCH_SIZE=64
+export KAGGLE_IMAGE_SIZE=256
+export KAGGLE_N_FOLD=4
+export WANDB_PROJECT=isic2020_infer_en_b5_mlp_0
+export KAGGLE_EXPERIMENT_NAME=en_b5_mlp_0
+
+#export KAGGLE_SANITY_CHECK=1
+
+PROJ_ROOT=$(python -c "from pathlib import Path; print(str(Path('$0').absolute().parent.parent.parent))")
+poetry run python ${PROJ_ROOT}/runner/infer_tta.py
