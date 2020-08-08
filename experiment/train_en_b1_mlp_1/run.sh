efe4dcaa7dc2fd46b24ea0ef1a6ebc882fe09726
Masahiro Wada <argon.argon.argon@gmail.com>
2020-08-08 16:22:47 +0900

    Add exps

---
+++ experiment/train_en_b1_mlp_1/run.sh
@@ -0,0 +1,23 @@
+#/usr/local/bin/bash
+FOLD_INDEX=${1}
+
+unset $(env | grep KAGGLE_ | awk -F = '{print $1}')
+
+export KAGGLE_MAX_EPOCHS=6
+export KAGGLE_LEARNING_RATE=0.001
+export KAGGLE_GPUS=1
+export KAGGLE_BATCH_SIZE=64
+export KAGGLE_IMAGE_SIZE=384
+export KAGGLE_LABEL_SMOOTHING=0.15
+export KAGGLE_POS_WEIGHT=4
+export KAGGLE_EARLY_STOP_PATIENCE=12
+export KAGGLE_N_FOLD=4
+export KAGGLE_TRAIN_FOLD_INDEX=${FOLD_INDEX}
+export WANDB_PROJECT=isic2020_train_en_b1_mlp
+export KAGGLE_EXPERIMENT_NAME=en_b1_mlp_0
+export KAGGLE_MODEL_NAME=en_b1_mlp
+
+#export KAGGLE_SANITY_CHECK=1
+
+PROJ_ROOT=$(python -c "from pathlib import Path; print(str(Path('$0').absolute().parent.parent.parent))")
+poetry run python ${PROJ_ROOT}/runner/runner/train_efficientnet_mlp.py
---
+++ experiment/train_en_b4_mlp_1/push.sh
@@ -0,0 +1,24 @@
+#/usr/local/bin/bash
+FOLD_INDEX=${1}
+
+unset $(env | grep KKT | awk -F = '{print $1}')
+
+export KKT_KAGGLE_MAX_EPOCHS=6
+export KKT_KAGGLE_LEARNING_RATE=0.001
+export KKT_KAGGLE_GPUS=1
+export KKT_KAGGLE_BATCH_SIZE=64
+export KKT_KAGGLE_IMAGE_SIZE=256
+export KKT_KAGGLE_LABEL_SMOOTHING=0.15
+export KKT_KAGGLE_POS_WEIGHT=4
+export KKT_KAGGLE_EARLY_STOP_PATIENCE=12
+export KKT_KAGGLE_N_FOLD=4
+export KKT_KAGGLE_TRAIN_FOLD_INDEX=${FOLD_INDEX}
+export KKT_WANDB_PROJECT=isic2020_train_en_b4_mlp
+export KKT_KAGGLE_EXPERIMENT_NAME=en_b4_mlp_0
+export KKT_KAGGLE_MODEL_NAME=en_b4_mlp
+
+#export KKT_KAGGLE_SANITY_CHECK=1
+
+PROJ_ROOT=$(python -c "from pathlib import Path; print(str(Path('$0').absolute().parent.parent.parent))")
+poetry run jupytext --to notebook  ${PROJ_ROOT}/runner/train_efficientnet_mlp.py
+poetry run kkt push --target .runner.train_en_mlp
